0979719b492206f506df81dacedf26b79da99bdf
pitershaoxia <978074571@qq.com>
2019-09-29 23:28:06 +0800

    项目

---
+++ src/main/java/com/cskaoyan/Project29527Application.java
@@ -0,0 +1,13 @@
+package com.cskaoyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project29527Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project29527Application.class, args);
+    }
+
+}
